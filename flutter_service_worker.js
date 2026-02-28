f992d9130c946bab80fa63242ff05c6f5bb2abb0
MicroSwitchers <141078536+MicroSwitchers@users.noreply.github.com>
2026-02-28 12:21:18 -0500

    Fix: call openSettings() so button editor shows on first open

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,4 +1,4 @@
-'use strict'; // build:1772298840
+'use strict'; // build:1772299278
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d6d96d41e2ab0f9307e332de567cd843
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "d3c5cec77cc4c0e3a796e17b987da29f",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "27339e1ede3e812339cd2ecaecf58c45",
+"flutter_bootstrap.js": "e4f951cc51d5d81e193e7f60931fec78",
 "icons/Icon-192.png": "6b90d3ae524fad7aff3d57415e26f687",
 "icons/Icon-512.png": "fafc64e9afeb9da39dae0c4aedad4c08",
 "icons/Icon-maskable-192.png": "6b90d3ae524fad7aff3d57415e26f687",
 "icons/Icon-maskable-512.png": "fafc64e9afeb9da39dae0c4aedad4c08",
 "index.html": "873f332945262324d5e1e02552ac8056",
 "/": "873f332945262324d5e1e02552ac8056",
-"main.dart.js": "c547bc4a167521238170284150684c6c",
+"main.dart.js": "5153df2424e055a3ef94cb3b6017d6b8",
 "manifest.json": "09f59e90deb3cc8111fd6c38ec691337",
 "version.json": "9045c5feefc485b0a293516356af7d5e"};
 // The application shell files that are downloaded before a service worker can
