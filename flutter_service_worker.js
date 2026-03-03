4d41b7ebb73f9e1f2208e6f5383af9e63041da11
MicroSwitchers <141078536+MicroSwitchers@users.noreply.github.com>
2026-03-02 19:00:04 -0500

    Deploy: sub-scan phrases feature

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "d6d96d41e2ab0f9307e332de567cd843
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "bfe75c09e3579456963601af6bd0f4d4",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "ddc5187a31b6d7874252b164e98764c7",
+"flutter_bootstrap.js": "a8c2996e43f2db81710d2bb8da8842fe",
 "icons/Icon-192.png": "fd8f4af9c192c07cbf270d1083764ad4",
 "icons/Icon-512.png": "a58f8645372482f93800ad57303493e9",
 "icons/Icon-maskable-192.png": "fd8f4af9c192c07cbf270d1083764ad4",
 "icons/Icon-maskable-512.png": "a58f8645372482f93800ad57303493e9",
 "index.html": "1529a46ff1f3cd075a576a735a23188c",
 "/": "1529a46ff1f3cd075a576a735a23188c",
-"main.dart.js": "6276a250c28d490d576928e0682cf9f7",
+"main.dart.js": "5c4abd8350e05a8638ff947d8a962177",
 "manifest.json": "bc7f22fd013958905f0a9afdb0a0653c",
 "version.json": "9045c5feefc485b0a293516356af7d5e"};
 // The application shell files that are downloaded before a service worker can
